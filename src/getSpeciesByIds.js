1fad52ab05305e5e2a65c18792316f7312eff2f4
Nathália <nathaliamarciaandrade@gmail.com>
2022-08-26 14:53:45 -0300

    Corrigindo o requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,9 +2,9 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  const idAnimais = species.filter((id) => 
-  ids.find((especies) => especies === id.id))
+  const idAnimais = species.filter((id) =>
+    ids.find((especies) => especies === id.id));
   return idAnimais;
 }
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
