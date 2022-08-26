12664d4c100a1b951ff5c938f43b45ccb544f755
Nathália <nathaliamarciaandrade@gmail.com>
2022-08-26 16:51:29 -0300

    Realiando o exercício 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const colaborador = employees.find((nome) =>
+    nome.firstName === employeeName || nome.lastName === employeeName);
+  return colaborador;
 }
+
+console.log(getEmployeeByName('Nigel'));
 
 module.exports = getEmployeeByName;
