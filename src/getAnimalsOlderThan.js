5ce7d883e2327f0b71cd8a476046c50c83d54123
Nathália <nathaliamarciaandrade@gmail.com>
2022-08-26 16:17:00 -0300

    Corrigindo o requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const nomeAnimais = species.find((animals) => animals.name === animal).residents;
+  const idadeAnimais = nomeAnimais.every((idade) => idade.age >= age);
+  return idadeAnimais;
 }
+
+console.log(getAnimalsOlderThan('lions', 4));
 
 module.exports = getAnimalsOlderThan;
