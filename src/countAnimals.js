de46e7f70a802e909e63ffcd2b45004843bdd9b8
Nathália <nathaliamarciaandrade@gmail.com>
2022-08-27 14:25:23 -0300

    Realizando o requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const contandoAnimais = species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+    return contandoAnimais;
+  }
+  if (animal.specie && animal.sex) {
+    const sexoAnimais = species.find((nome) => nome.name === animal.specie).residents
+      .filter((nome) => nome.sex === animal.sex).length;
+    return sexoAnimais;
+  }
+  const quantAnimais = species.find((nome) => nome.name === animal.specie).residents.length;
+  return quantAnimais;
 }
+
+console.log(countAnimals({ specie: 'penguins', sex: 'female' }));
 
 module.exports = countAnimals;
